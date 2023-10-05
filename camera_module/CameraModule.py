32af5539abda2e742233a22053ea106ac3076fa5
juanriquelmer <juan.riquelmer@usm.cl>
2023-10-05 20:00:46 -0300

    Avance en el calculo de las métricas en el módulo de la camara.

--- camera_module/CameraModule.py
+++ camera_module/CameraModule.py
@@ -1,6 +1,9 @@
 import os
 import time
 import cv2
+from scipy import stats
+from skimage.metrics import structural_similarity as ssim
+import numpy as np
 
 
 class CameraModule:
@@ -26,6 +29,39 @@ class CameraModule:
         """
         photo_name = f"{photo_number:03d}{self.photo_format}"
         return os.path.join(self.photo_directory, photo_name)
+    
+    def _calculate_metrics(self, image_np):
+        # Varianza del histograma de luminosidad
+        variance = np.var(image_np)
+        
+        # Entropía de la imagen
+        hist = cv2.calcHist([image_np], [0], None, [256], [0, 256])
+        hist = hist.ravel() / hist.sum()
+        entropy = stats.entropy(hist)
+        
+        return variance, entropy
+
+    def _mse(self, image_a, image_b):
+        # Calcular el error cuadrático medio entre dos imágenes
+        err = np.sum((image_a.astype("float") - image_b.astype("float")) ** 2)
+        err /= float(image_a.shape[0] * image_a.shape[1])
+        return err
+    
+    def _compare_images(self, image_np, reference_image_np):
+        s = ssim(image_np, reference_image_np)
+        e = self._mse(image_np, reference_image_np)
+        return s, e
+
+    def _process_image(self, image_path):
+        image_np = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
+        variance, entropy = self._calculate_metrics(image_np)
+        
+        print(f"Métricas para {image_path}:")
+        print(f"  Varianza: {variance}")
+        print(f"  Entropía: {entropy}")
+
+        if variance < 40:  
+            print(f"Advertencia: La imagen {image_path} podría estar oscurecida.")
 
     def save_photo(self, photo_number):
         """
@@ -36,8 +72,9 @@ class CameraModule:
         if ret:
             cv2.imwrite(photo_path, frame)
             print(f"Foto {photo_path} guardada.")
+            self._process_image(photo_path)
         else:
-            print("Error capturing photo.")
+            print("Error capturando la foto.")
             self._handle_error()
 
     def delete_photo(self, photo_number):
@@ -58,7 +95,7 @@ class CameraModule:
         """
         self.cap = cv2.VideoCapture(self.camera_index)
         if not self.cap.isOpened():
-            print("Error abriendo la cámara.")
+            print("Error al abrir la cámara.")
             self._handle_error()
             return
 
@@ -68,6 +105,7 @@ class CameraModule:
             time.sleep(self.capture_period)
 
         self.cap.release()
+    
 
     def _handle_error(self):
         """
